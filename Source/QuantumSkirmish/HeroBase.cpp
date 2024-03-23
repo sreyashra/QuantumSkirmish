2273217563d8a6d31da8af168b4bffe58d04651d
Sreyash Rajalingam Ananth <rasreyash@gmail.com>
2024-03-23 20:21:20 +0000

    IMC_Player created
    Input Mapping Context created for player

--- Source/QuantumSkirmish/HeroBase.cpp
+++ Source/QuantumSkirmish/HeroBase.cpp
@@ -2,12 +2,23 @@
 
 
 #include "HeroBase.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "EnhancedInputComponent.h"
 
 // Sets default values
 AHeroBase::AHeroBase()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	//Create a camera boom
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+
+	//Create a folow camera
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
 
 }
 
@@ -28,7 +39,10 @@ void AHeroBase::Tick(float DeltaTime)
 // Called to bind functionality to input
 void AHeroBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+
+	}
 
 }
 
--- Source/QuantumSkirmish/HeroBase.h
+++ Source/QuantumSkirmish/HeroBase.h
@@ -6,6 +6,10 @@
 #include "GameFramework/Character.h"
 #include "HeroBase.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+
 UCLASS()
 class QUANTUMSKIRMISH_API AHeroBase : public ACharacter
 {
@@ -15,15 +19,24 @@ public:
 	// Sets default values for this character's properties
 	AHeroBase();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* PlayerMappingContext;
 
 };
