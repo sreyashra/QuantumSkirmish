4ae5d6fba200ba94c2196b6974c43742e3380610
Sreyash Rajalingam Ananth <rasreyash@gmail.com>
2024-03-23 22:34:44 +0000

    Move and Look mechanic
    Player movement and look mechanics complete in HeroBase class

--- Source/QuantumSkirmish/HeroBase.h
+++ Source/QuantumSkirmish/HeroBase.h
@@ -9,6 +9,8 @@
 class USpringArmComponent;
 class UCameraComponent;
 class UInputMappingContext;
+class UInputAction;
+struct FInputActionValue;
 
 UCLASS()
 class QUANTUMSKIRMISH_API AHeroBase : public ACharacter
@@ -29,14 +31,41 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-private:	
+public:
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+
+	/** Returns FollowCamera subobject **/
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+protected:
+	//Called for movement input
+	void Move(const FInputActionValue& Value);
+
+	//Called for look input
+	void Look(const FInputActionValue& Value);
+
+private:
+	/** Camera boom */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
 	USpringArmComponent* CameraBoom;
 
+	/** Follow camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* FollowCamera;
 
+	/** Mapping Context */
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
 	UInputMappingContext* PlayerMappingContext;
 
+	/* Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveAction;
+
+	/** Look Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	UInputAction* LookAction;
+
+	/** Player Controller */
+	APlayerController* PlayerController;
 };
