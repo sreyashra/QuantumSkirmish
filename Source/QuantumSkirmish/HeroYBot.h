975875e85f7101bd1a52d4490b4f73075faa3150
Sreyash Rajalingam Ananth <rasreyash@gmail.com>
2024-03-24 00:56:30 +0000

    HeroYBot class
    HeroYBot C++ child class created

---
+++ Source/QuantumSkirmish/HeroYBot.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HeroBase.h"
+#include "HeroYBot.generated.h"
+
+DECLARE_LOG_CATEGORY_EXTERN(LogHeroYBotCharacter, Log, All);
+
+UCLASS(Config = Game)
+class QUANTUMSKIRMISH_API AHeroYBot : public AHeroBase
+{
+	GENERATED_BODY()
+	
+};
